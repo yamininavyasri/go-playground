b5961bd3374c4f33cb0319be9fb536f12e7ccefe
yaminivenkat.grandhi <yaminivenkat.grandhi@servicenow.com>
2025-10-09 15:30:32 +0530

    modified float variable

--- variable_types/variable_types.go
+++ variable_types/variable_types.go
@@ -10,5 +10,5 @@ func main() {
 	y := 1.0
 
 	// Print with 1 decimal for float
-	fmt.Printf("Declared variables are: %d, %s, %f\n", x, name, y)
+	fmt.Printf("Declared variables are: %d, %s, %0.1f\n", x, name, y)
 }
